da7e650237d031a25ecae062edfacd5ad7a12fb3
ijosephs Consutling <joe@ijosephs.com>
2014-08-03 14:26:56 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+BookRags Project, View
+Joe Smith
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>BookRags Assignment Joe Smith</title>
+        <link href="main.css" type="text/css" rel="stylesheet" >
+        <?PHP require ("BookRagsLib.php"); ?>
+        
+    </head>
+    <body>
+        <div id="column_wrapper" class="colwrapper">View Images
+             
+               <?php $RssObj1= new RSSFeed();?>
+               
+        </div>
+    </body>
+</html>
